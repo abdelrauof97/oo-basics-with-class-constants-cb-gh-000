a9395b02ac2ceea4eeafff779abc2f593ea1e9c5
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-14 16:10:11 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
-  
+  GENERS = []
   def initialize(title)
     @title = title
   end
@@ -9,7 +9,12 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  
+  def genre=(genre)
+    @genre = genre 
+    GENERS << genre
+  end
+  
 end
 
-# 
+# learn spec/01_book_spec.rb
