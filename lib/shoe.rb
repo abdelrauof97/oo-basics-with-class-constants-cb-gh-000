7e54e5583cb33746ce749ff23b1e73885647f0f9
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-14 16:47:55 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  
+  attr_reader :brand
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand unless BRANDS.include(brand)
     
   end
 
@@ -12,10 +12,7 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  def brand 
-    BRANDS.uniq.flaten.compact
-  end
+
 
 end
 
